856d2cc9fb1ccf14e11a763ef6943032a3cdb293
newthiagoassisk8qw <thiago.silva@qwsoftware.com.br>
2026-08-26 17:26:04 -0300

    classes deathNote rule para praticar composicao

---
+++ src/Model/DeathNote.java
@@ -0,0 +1,15 @@
+
+public class DeathNote {
+    public int numberOfVictims;
+    public String owner;
+    public boolean isDestroyed;
+    public Rule rules;
+
+    public DeathNote(int numberOfVictims, String owner) {
+        this.numberOfVictims = numberOfVictims;
+        this.owner = owner;
+    }
+
+
+
+}
---
+++ src/Model/Rule.java
@@ -0,0 +1,11 @@
+public class Rule {
+   public String name;
+   public int defaultTimeDeath;
+
+    public Rule(String name, int defaultTimeDeath) {
+        this.name = name;
+        this.defaultTimeDeath = defaultTimeDeath;        
+    }
+    
+
+}
