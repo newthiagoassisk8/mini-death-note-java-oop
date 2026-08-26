1e6bb90f4df3d3a16229b18e5debc6c86239854d
newthiagoassisk8qw <thiago.silva@qwsoftware.com.br>
2026-08-26 17:50:42 -0300

    novas classes

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@
 // TODO: O que acontece quando mais de uma thread ela tenta alterar o mesmo valor
 // TODO: Relacionamento entre objetos
 // TODO: Aprofundar Interface
+import Model.DeathNote;
 import Model.DeathNoteUser;
+import Model.Rule;
 import Model.Victim;
 
 public class Main {
@@ -13,6 +15,8 @@ public class Main {
         Victim matsuda = new Victim("Mastuda", "Heart Attack");
         // Aplicacao de polimorfismo
         light.writeInDeathNote(matsuda);
+        Rule foo = new Rule("regra1", 40);
+        DeathNote bar = new DeathNote(2, "Ryuk", foo);
         System.out.println(matsuda.toString());
     }
     
